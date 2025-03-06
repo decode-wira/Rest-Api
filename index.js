7d8d8f1cd268725148df6e7fdeabc72da8a0cc98
Wira Liwirya <tampanwira53@gmail.com>
2025-03-06 18:34:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const { updateUsage, tambahPengunjung, databaseMiddleware, fetchGitHubFile, getDatabase, saveDatabase, authenticateToken, getTodayDate } = require('./lib/untils')
 require("dotenv").config();
+app.set("json spaces", 2);
 
 const app = express();
 app.use(express.json());
